1a2f8fcbd561bf01394cd8196eaaef2c72049d17
van-reebeck <98661931+van-reebeck@users.noreply.github.com>
2022-02-25 20:26:00 +0200

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,13 +10,13 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://i.imgur.com/232HsEg.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `𝑪𝒐𝒔𝒎𝒊𝒄 𝒃𝒐𝒕 CODDED BY KHADHER
-Creator number 👉: wa.me/917025191792
+Creator number 👉: wa.me/27634923280
 
-Githublink (Setup)  👇:    https://github.com/khadher05/COSMIC
+Githublink (Setup)  👇: github.com/van-reebeck/TSHEPHANG
 
-DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/khadher05/COSMIC.git
+DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/van-reebeck/TSHEPHANG.git
 
-PUBLIC BOT GROUP 👇: https://chat.whatsapp.com/FhPPp43dYlNLi499ZJuq5X
+PUBLIC BOT GROUP 👇: Follow this link to join my WhatsApp group: https://chat.whatsapp.com/CHWAJyGYCKG17UMF21A42I
 
 
 `}) 
