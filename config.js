dcf5f87ad714f038af9f3d3883277689658ee968
van-reebeck <98661931+van-reebeck@users.noreply.github.com>
2022-02-26 10:07:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2021 phaticusthiccy
-re-edited by KINGS AS
+re-edited by TSHEPHANG
 */
 
 const { Sequelize } = require('sequelize');
@@ -63,7 +63,7 @@ module.exports = {
     DATABASE: DATABASE_URL === './whatsasena.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
     RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
     NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
-    SUDO: process.env.SUDO === undefined ? '27634923280,0' : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? '27634923280' : process.env.SUDO,
     DEBUG: DEBUG,
     COFFEEHOUSE_API_KEY: process.env.COFFEEHOUSE_API_KEY === undefined ? false : process.env.COFFEEHOUSE_API_KEY,
     WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG",
